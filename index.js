22580aa05dfba3e486af4ce1d87280220c85784a
tatang <tatang.romadhona@rushowl.sg>
2022-10-19 21:21:10 +0700

    delete sequelize

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const { User } = require('./models');
 const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator');
 const morgan = require('morgan');
+const fs = require('fs');
+const data = require('./data.json');
 
 app.use(express.json());
 app.use(morgan('dev'));
@@ -19,15 +20,13 @@ app.get('/', (req, res) => {
 app.get('/add-data', async (req, res, next) => {
     try {
         const randomName = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] }); // big_red_donkey
-
-        const user = await User.create({
-            name: randomName
-        });
+        data.names.push(randomName);
+        fs.writeFileSync('./data.json', JSON.stringify(data));
 
         return res.status(200).json({
             status: true,
             message: 'berhasil',
-            data: user.name
+            data: randomName
         });
     } catch (err) {
         next(err);
@@ -36,12 +35,10 @@ app.get('/add-data', async (req, res, next) => {
 
 app.get('/get-data', async (req, res, next) => {
     try {
-        const user = await User.findAll();
-
         return res.status(200).json({
             status: true,
             message: 'berhasil',
-            data: user
+            data: data.names
         });
     } catch (err) {
         next(err);
